f5d0c392e8654d98bbc3700336e36a2141f66dc0
Martin Liska <mliska@suse.cz>
2020-11-12 09:22:07 +0100

    Update.

--- bugzilla-cves.py
+++ bugzilla-cves.py
@@ -7,7 +7,7 @@ binutils_bugzilla_regex = re.compile(r'https:\/\/sourceware.org\/bugzilla\/show_
 binutils_bugzilla_prefix = 'https://sourceware.org/bugzilla/show_bug.cgi?id='
 opensuse_bugzilla_prefix = 'https://bugzilla.opensuse.org/show_bug.cgi?id='
 pr_regex = re.compile(r'\+\s+PR ([\w-]+\/)?([0-9]+)')
-obs_changelog = '/home/marxin/BIG/osc/home:marxin:branches:devel:gcc/binutils/binutils.changes'
+obs_changelog = '/home/marxin/BIG/osc/home:marxin:branches:devel:gcc-new/binutils/binutils.changes'
 
 # the file is created from bugzilla with Export XML from all PRs assigned to matz@suse.com
 
